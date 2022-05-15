d023a3ca4fab61f8199fea6bdc2094ddc72a802b
ASHGRAY-DK\imcha <imchangkyu@gmail.com>
2022-05-15 22:15:46 +0900

    Initial Commit

---
+++ Assets/Scripts/HitTarget.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitTarget : MonoBehaviour,IDamageable {
+	public float hp = 100f;
+	public Animator animator;
+
+	private void Awake() {
+		animator = GetComponent<Animator>();
+	}
+
+	private void Update() {
+		if(animator.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.Die")
+		&& animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f){
+			Destroy(gameObject);
+		}
+	}
+
+	public void OnDamage(float damage)
+	{
+		hp -= damage;
+
+		if(hp <= 0)
+		{
+			animator.SetTrigger("Die");
+		}
+	}
+}
